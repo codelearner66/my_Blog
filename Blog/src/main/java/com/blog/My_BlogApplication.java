790e9fed1b364767ff39099b4dda6686bc320c13
codelearner66 <61338215+codelearner66@users.noreply.github.com>
2022-03-22 23:11:27 +0800

    基础项目后端

---
+++ Blog/src/main/java/com/blog/My_BlogApplication.java
@@ -0,0 +1,11 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My_BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(My_BlogApplication.class,args);
+    }
+}
